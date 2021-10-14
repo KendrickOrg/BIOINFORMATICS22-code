4f7f188e085e5441fe62adba8fb38a46dcfaf2f8
Yvan Guifo <56076081+YvanGuifo@users.noreply.github.com>
2021-10-14 09:30:19 +0100

    Update Plot_Exponential_Network.py

--- Plot_Exponential_Network.py
+++ Plot_Exponential_Network.py
@@ -6,95 +6,57 @@ import networkx as nx
 import pandas as pd
 import random
 
+N = 10**4
+kave = 10
 
-x = np.arange(-0.1, 200, 0.1)
-
-
-colors = ['#5AB3E6','#FF2000','#009A80','#E69A00', '#CD9AB3', '#0073B3','#F0E442']
-
-rho = 0.0001
-tau = 0.025 #transmission rate
-gamma = 0.1 
-Nbig=10000
-
-iterations = 50
+tmin = 0
 tmax = 200
+simulationRuns = 50
 
-PlPk = {}
-exponent = 9.49
+tau = 0.025    #transmission rate
+gamma = 0.1     #recovery rate
+rho = 0.005
+step = 0.1     #step size
 
-for k in range(1,10000):
-    PlPk[k]= (1-np.exp(-1./exponent))*(np.exp(-1*(k-1)/exponent))
-    kave = 10
+step_max_plot = 1200
 
-normfact= sum(PlPk.values())
-for k in PlPk:
-    PlPk[k] /= normfact
+xx = np.arange(0, step_max_plot*step, step)
+yy = np.arange(0, step_max_plot*step, step)
 
-def PsiPowLaw(x):
-    #print PlPk
-    rval = 0
-    for k in PlPk:
-        rval += PlPk[k]*x**k
-    return rval
+plt.figure(figsize=(12,8))
 
-def DPsiPowLaw(x):
-    rval = 0
-    for k in PlPk:
-        rval += k*PlPk[k]*x**(k-1)
-    return rval
-
-def get_G(N, Pk):
-    while True:
-        ks = []
-        for ctr in range(N):
-            r = random.random()
-            for k in Pk:
-                if r<Pk[k]:
-                    break
-                else:
-                    r-= Pk[k]
-            ks.append(k)
-        if sum(ks)%2==0:
-            break
-    G = nx.configuration_model(ks)
-    return G
+nb_initial_infecteds = 50
+initial_infecteds = range(nb_initial_infecteds)
 
 
-def process_degree_distribution(Gbig,  color, Psi, DPsi, symbol):
-    for counter in range(iterations): 
-        t, S, I1, R = EoN.fast_SIR(Gbig, tau, gamma, rho=rho, tmax = tmax)
-        if counter == 0:
-            plt.plot(t, I1*1./Gbig.order(), color = 'gray', alpha=0.3, label='Individual simulation run')
-        plt.plot(t, I1*1./Gbig.order(), color = 'gray', alpha=0.3)
-
-Gbig = get_G(Nbig, PlPk)
-process_degree_distribution(Gbig,  colors[3], PsiPowLaw, DPsiPowLaw, 'd')
 
 
-df1 = pd.read_csv('Sources/Aparicio_ExpoS.csv')
+df1 = pd.read_csv('Aparicio_ExpoS.csv')
 df1 = df1.loc[:, '{#status->#S}']
-inc1 = - df1.diff()*11.5
+df1 = df1*N
+inc1 = - df1.diff()/0.1
+inc1 = inc1[:step_max_plot]
 
-
-df2 = pd.read_csv('Sources/Stroud_HMS.csv')
+df2 = pd.read_csv('Stroud_HMS.csv')
 df2 = df2.loc[:, '{#status->#S}']
-inc2 = - df2.diff()*60
+df2 = df2*N
+inc2 = - df2.diff()/0.1
+inc2 = inc2[:step_max_plot]
 
-
-df3 = pd.read_csv('Sources/Stroud_ExpoS.csv')
+df3 = pd.read_csv('Stroud_ExpoS.csv')
 df3 = df3.loc[:, '{#status->#S}']
-inc3 = - df3.diff()*50
+df3 = df3*N
+inc3 = - df3.diff()/0.1
+inc3 = inc3[:step_max_plot]
 
-
-plt.plot(x, inc1, '--', label='Aparicio simulation', color='green')
-plt.plot(x, inc2, label='Homogeneous Mixing', color='red')
-plt.plot(x, inc3, '-.', label='Stroud simulation', color='blue')
+plt.plot(xx, inc1, '--', label = 'Aparicio simulation', color = 'green')
+plt.plot(xx, inc2, label = 'Homogeneous mixing simulation', color = 'red')
+plt.plot(xx, inc3, '-.', label = 'Stroud simulation', color = 'blue')
 
 plt.xlabel('Time')
 plt.ylabel('New infections per day')
-plt.title('Exponential Network')
+plt.title('Exponential network')
 plt.legend()
-
 plt.show()
-plt.savefig('Plot/Exponential_network.png')
+plt.savefig('Plot/Expo.png')
+
