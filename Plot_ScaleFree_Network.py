74eeb877552a2c8bae23a594d5aee4940fd8ea69
Yvan Guifo <56076081+YvanGuifo@users.noreply.github.com>
2021-10-21 16:34:32 +0200

    Update Plot_ScaleFree_Network.py

--- Plot_ScaleFree_Network.py
+++ Plot_ScaleFree_Network.py
@@ -25,25 +25,26 @@ xx = np.arange(0, step_max_plot*step, step)
 yy = np.arange(0, step_max_plot*step, step)
 
 plt.figure(figsize=(12,8))
+plt.ylim(0.0,400)
 
 nb_initial_infecteds = 50
 initial_infecteds = range(nb_initial_infecteds)
     
 df1 = pd.read_csv('Stock/Aparicio_ScaleFreS.csv')
 df1 = df1.loc[:, '{#status->#S}']
-df1 = df1*N
+
 inc1 = - df1.diff()/0.1
 inc1 = inc1[:step_max_plot]
 
 df2 = pd.read_csv('Stock/Stroud_HMS.csv')
 df2 = df2.loc[:, '{#status->#S}']
-df2 = df2*N
+
 inc2 = - df2.diff()/0.1
 inc2 = inc2[:step_max_plot]
 
 df3 = pd.read_csv('Stock/Stroud_ScaleS.csv')
 df3 = df3.loc[:, '{#status->#S}']
-df3 = df3*N
+
 inc3 = - df3.diff()/0.1
 inc3 = inc3[:step_max_plot]
 
