3220ab945c9846985bc4f27c58c57145a4fdbd6f
Yvan Guifo <56076081+YvanGuifo@users.noreply.github.com>
2021-10-29 07:31:01 +0200

    Update Plot_ScaleFree_Network.py

--- Plot_ScaleFree_Network.py
+++ Plot_ScaleFree_Network.py
@@ -4,20 +4,21 @@ import numpy as np
 import EoN
 import networkx as nx
 import pandas as pd
-import scipy.interpolate
-from collections import defaultdict
 import random
 
+
+
 N = 10**4
-G = nx.barabasi_albert_graph(N, 10)
+kave = 10
+
 tmin = 0
 tmax = 200
 simulationRuns = 50
+
 tau = 0.025    #transmission rate
 gamma = 0.1     #recovery rate
 rho = 0.005
 step = 0.1     #step size
-kave = 10
 
 step_max_plot = 1200
 
@@ -29,20 +30,21 @@ plt.ylim(0.0,400)
 
 nb_initial_infecteds = 50
 initial_infecteds = range(nb_initial_infecteds)
-    
-df1 = pd.read_csv('Stock/Aparicio_ScaleFreS.csv')
+
+
+df1 = pd.read_csv('/Users/admin/Desktop/Stock/Aparicio_ScaleFree.csv')
 df1 = df1.loc[:, '{#status->#S}']
 
 inc1 = - df1.diff()/0.1
 inc1 = inc1[:step_max_plot]
 
-df2 = pd.read_csv('Stock/Stroud_HMS.csv')
+df2 = pd.read_csv('/Users/admin/Desktop/Stock/Stroud_HMS.csv')
 df2 = df2.loc[:, '{#status->#S}']
 
 inc2 = - df2.diff()/0.1
 inc2 = inc2[:step_max_plot]
 
-df3 = pd.read_csv('Stock/Stroud_ScaleS.csv')
+df3 = pd.read_csv('/Users/admin/Desktop/Stock/Stroud_ScaleS.csv')
 df3 = df3.loc[:, '{#status->#S}']
 
 inc3 = - df3.diff()/0.1
@@ -54,9 +56,8 @@ plt.plot(xx, inc3, '-.', label = 'Stroud simulation', color = 'blue')
 
 plt.xlabel('Time')
 plt.ylabel('New infections per day')
-plt.title('Scale Free Network')
+plt.title('Scale Free network')
 plt.legend()
 plt.show()
-plt.savefig('Plot/ScaleFreeF.png')
-    
+plt.savefig('Plot/ScaleFree.png')
 
